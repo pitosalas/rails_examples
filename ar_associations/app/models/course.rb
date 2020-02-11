60a9a65908428f3a1ec0c5810c8866e617304255
Pito Salas <pitosalas@gmail.com>
2020-02-11 17:10:09 -0500

    checkpoint

--- ar_associations/app/models/course.rb
+++ ar_associations/app/models/course.rb
@@ -2,7 +2,6 @@ class Course < ApplicationRecord
   has_many :registrations
   has_many :students, through: :registrations
 
-
   has_many :hirings
   has_many :teaching_assistants, through: :hirings, source: :student
 
